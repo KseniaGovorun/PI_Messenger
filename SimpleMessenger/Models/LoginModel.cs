188f8b4a675beb42cc615bd0c98b653ddeb06792
NastiaChaikovska <chaikovska2002@gmail.com>
2023-05-16 21:15:28 +0300

    create login model

--- SimpleMessenger/Models/LoginModel.cs
+++ SimpleMessenger/Models/LoginModel.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleMessenger.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Email не вказано")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Пароль не вказано")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
