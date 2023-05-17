055a327344d18681038c7d451177c4333980160b
MHatalyak <matronahu@gmail.com>
2023-05-17 21:47:02 +0300

    added conversation model

--- SimpleMessenger/Models/Conversation.cs
+++ SimpleMessenger/Models/Conversation.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System.Collections.Generic;
+
+namespace SimpleMessenger.Models
+{
+    public class Conversation
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int ConversationTypeID { get; set; } 
+        public ConversationType ConversationType { get; set; }
+        public ICollection<UserConversation> UserConversations { get; set; }
+        public ICollection<Message> Messages { get; set; }
+    }
+}
