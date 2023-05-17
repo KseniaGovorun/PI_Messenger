8640483488b492597a04583029e01bcbcb8b811b
MHatalyak <matronahu@gmail.com>
2023-05-17 21:49:55 +0300

    added conversation_type model

--- SimpleMessenger/Models/ConversationType.cs
+++ SimpleMessenger/Models/ConversationType.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System.Collections.Generic;
+
+namespace SimpleMessenger.Models
+{
+    public class ConversationType
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public ICollection<Conversation> Conversations { get; set; }
+    }
+}
